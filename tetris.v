f6a4b62846f345654b8ced531fd4dd65aceb012c
Willmuska <31712601+Willmuska@users.noreply.github.com>
2018-11-27 00:12:20 -0500

    Added Chris' files

--- tetris.v
+++ tetris.v
@@ -3,7 +3,8 @@ input clock_on_board,
 input start_game,
 input resetn,
 input key_left,
-input key_right);
+input key_right,
+input key_rotate);
 
 	// An array that contains the status of each location in the board, and whether there is an already dropped
 	// block filling that coordinate.
@@ -13,9 +14,17 @@ input key_right);
 	reg [3:0]load_x;
    reg [4:0]load_y;
 	
+	// The block type and rotation state.
+	reg [2:0]block_type;
+	reg [2:0]rotation;
+	
 	// The x and y positions of the four blocks of the tetromino.
 	wire [4:0] block1_y, block2_y, block3_y, block4_y;
 	wire [3:0] block1_x, block2_x, block3_x, block4_x;
+	
+	// The x and y positions of the four blocks of the tetromino, if it were rotated.
+	wire [4:0] block1_y_test, block2_y_test, block3_y_test, block4_y_test;
+	wire [3:0] block1_x_test, block2_x_test, block3_x_test, block4_x_test;
 	
 	// The x and y positions of the tetromino's central block.
 	reg [4:0] y;
@@ -40,6 +49,8 @@ input key_right);
 	block_returner b1(
 	.x(x),
 	.y(y),
+	.block_type(0),
+	.rotation(0),
 	.x1(block1_x),
 	.y1(block1_y),
 	.x2(block2_x),
@@ -48,6 +59,21 @@ input key_right);
 	.y3(block3_y),
 	.x4(block4_x),
 	.y4(block4_y));
+	
+	// Returns the four blocks of the next rotation
+	block_returner b2(
+	.x(x),
+	.y(y),
+	.block_type(0),
+	.rotation(0),
+	.x1(block1_x_test),
+	.y1(block1_y_test),
+	.x2(block2_x_test),
+	.y2(block2_y_test),
+	.x3(block3_x_test),
+	.y3(block3_y_test),
+	.x4(block4_x_test),
+	.y4(block4_y_test));
 	
 	// Returns a 60Hz (approximately) clock.
 	rate_divider r1(
@@ -86,6 +112,17 @@ input key_right);
 		end
 	endtask
 	
+	// Rotates the tetromino clockwise.
+	task rotate(); 
+		begin
+			if (rotation < 3) begin
+				rotation <= rotation + 1;
+			end else begin
+				rotation <= 0;
+			end
+		end
+	endtask
+	
 	// Fills in the board state with the current coordinates of the four blocks.
 	task update_board();
 		begin
@@ -111,6 +148,19 @@ input key_right);
 	wire filled_right = (block1_x == 9 || block2_x == 9 || block3_x == 9 || block4_x == 9) || 
 	(board_state[block1_y][block1_x + 1] || board_state[block2_y][block2_x + 1] 
 	|| board_state[block3_y][block3_x + 1] || board_state[block4_y][block4_x + 1]);
+	
+	// Whether any of the four blocks that would result from a rotation would be out of bounds.
+	wire rotation_out_of_bounds = (!(block1_y_test >= 0 && block1_y_test < 23)) || ((block2_y_test >= 0 && block2_y_test < 23) 
+	|| !(block3_y_test >= 0 && block3_y_test < 23) || !(block4_y_test >= 0 && block4_y_test < 23))
+	|| (!(block1_x_test >= 0 && block1_x_test < 10)) || ((block2_x_test >= 0 && block2_x_test < 10) 
+	|| !(block3_x_test >= 0 && block3_x_test < 10) || !(block4_x_test >= 0 && block4_x_test < 10));
+	
+	// Whether any of the four blocks that would result from a rotation would be intersecting fallen blocks.
+	wire rotation_intersects_existing = (board_state[block1_y_test][block1_x_test] || board_state[block2_y_test][block2_x_test] 
+	|| board_state[block3_y_test][block3_x_test] || board_state[block4_y_test][block4_x_test]);
+	
+	// Whether any of the four blocks that would result from a rotation would conflict with boundaries.
+	wire rotation_conflicts = rotation_out_of_bounds || rotation_intersects_existing;
 	
 	control c1(.clock(clock_block_fall),
 	.start_game(start_game),
@@ -145,6 +195,8 @@ input key_right);
 		// Checks if the user wants to move to the right.
 		end else if (key_right && !filled_right) begin
 			move_right();
+		end else if (key_rotate && !rotation_conflicts) begin
+			rotate();
 		end
 	end
 endmodule
